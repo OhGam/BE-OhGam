0368e0f1b4e31a922957db5907fe1ef96acf24b8
SongJSeop <wnstjq7979@gmail.com>
2023-06-29 16:11:04 +0900

    init: 스프링 프로젝트 생성

---
+++ src/main/java/chbbo/BEOhGam/BeOhGamApplication.java
@@ -0,0 +1,13 @@
+package chbbo.BEOhGam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOhGamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOhGamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chbbo/BEOhGam/BeOhGamApplicationTests.java
@@ -0,0 +1,13 @@
+package chbbo.BEOhGam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeOhGamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
