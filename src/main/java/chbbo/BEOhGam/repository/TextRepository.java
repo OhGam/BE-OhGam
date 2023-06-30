50e44b7fba0d85db306bf945b975326749968474
SongJSeop <wnstjq7979@gmail.com>
2023-06-30 16:44:35 +0900

    feat: TextRepository 구현 및 테스트코드 작성

---
+++ src/main/java/chbbo/BEOhGam/repository/TextRepository.java
@@ -0,0 +1,7 @@
+package chbbo.BEOhGam.repository;
+
+import chbbo.BEOhGam.domain.Text;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TextRepository extends JpaRepository<Text, Long> {
+}
--- src/test/java/chbbo/BEOhGam/repository/NoteRepositoryTest.java
+++ src/test/java/chbbo/BEOhGam/repository/NoteRepositoryTest.java
@@ -1,11 +1,14 @@
 package chbbo.BEOhGam.repository;
 
 import chbbo.BEOhGam.domain.Note;
+import chbbo.BEOhGam.domain.Text;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -15,6 +18,8 @@ public class NoteRepositoryTest {
 
     @Autowired
     NoteRepository noteRepository;
+    @Autowired
+    TextRepository textRepository;
 
     @Test
     @Transactional
@@ -29,5 +34,38 @@ public class NoteRepositoryTest {
         // then
         assertThat(foundNote).isNotNull();  // foundNote에 Note가 담겨져 있는지
         assertThat(foundNote).isSameAs(note);  // 담긴 Note가 note와 같은지
+    }
+
+    @Test
+    @Transactional
+    void textAddTest() {
+        // given
+        Note note = new Note();
+        Text text1 = new Text();
+        text1.setContent("감사1");
+        text1.setNote(note);
+        Text text2 = new Text();
+        text2.setContent("감사2");
+        text2.setNote(note);
+        Text text3 = new Text();
+        text3.setContent("감사3");
+        text3.setNote(note);
+        List<Text> text = new ArrayList<>();
+        text.add(text1);
+        text.add(text2);
+        text.add(text3);
+        note.setText(text);
+
+        // when
+        textRepository.save(text1);
+        textRepository.save(text2);
+        textRepository.save(text3);
+        noteRepository.save(note);
+        Note foundNote = noteRepository.findById(note.getId()).get();
+
+        // then
+        for (int i = 0; i < foundNote.getText().size(); i++) {
+            assertThat(foundNote.getText().get(i).getContent()).isEqualTo("감사" + (i+1));
+        }
     }
 }
