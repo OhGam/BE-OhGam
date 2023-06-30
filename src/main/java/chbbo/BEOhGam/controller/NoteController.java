2a54c72259069c540fe7a74ee8029f4d8a82ad45
SongJSeop <wnstjq7979@gmail.com>
2023-07-01 02:34:50 +0900

    Feat: NoteController noteIndex 추가

--- src/main/java/chbbo/BEOhGam/controller/NoteController.java
+++ src/main/java/chbbo/BEOhGam/controller/NoteController.java
@@ -1,4 +1,13 @@
 package chbbo.BEOhGam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class NoteController {
+
+    @GetMapping("/Note")
+    public String noteIndex() {
+        return "noteIndex";
+    }
 }
