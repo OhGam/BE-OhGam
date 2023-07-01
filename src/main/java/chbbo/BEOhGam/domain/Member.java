98b661a9f892a56f01c863bdf4b8641e46b8e058
pia01190 <pia01190@naver.com>
2023-07-01 21:24:52 +0900

    Member 도메인 구현

--- src/main/java/chbbo/BEOhGam/domain/Member.java
+++ src/main/java/chbbo/BEOhGam/domain/Member.java
@@ -1,4 +1,30 @@
 package chbbo.BEOhGam.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "db_member")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String username;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String nickname;
+
+
 }
