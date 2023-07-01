960b790e8e9359ac547f35bfa669642191f22658
pia01190 <pia01190@naver.com>
2023-07-01 21:33:42 +0900

    MemberRepository 구현

--- src/main/java/chbbo/BEOhGam/controller/MemberController.java
+++ src/main/java/chbbo/BEOhGam/controller/MemberController.java
@@ -1,4 +1,20 @@
 package chbbo.BEOhGam.controller;
 
+import chbbo.BEOhGam.domain.Member;
+import chbbo.BEOhGam.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "/member/createMemberForm";
+    }
 }
--- src/main/java/chbbo/BEOhGam/repository/MemberRepository.java
+++ src/main/java/chbbo/BEOhGam/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package chbbo.BEOhGam.repository;
 
-public interface MemberRepository {
+import chbbo.BEOhGam.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
