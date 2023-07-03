7b275f2d3b878d0bc2b7030f7e95a00c1900d16f
SongJSeop <wnstjq7979@gmail.com>
2023-07-04 02:34:02 +0900

    Refactor: 모든 Note 조회 api에서 Note를 NoteDTO로 대체

--- src/main/java/chbbo/BEOhGam/api/NoteAPIController.java
+++ src/main/java/chbbo/BEOhGam/api/NoteAPIController.java
@@ -1,6 +1,7 @@
 package chbbo.BEOhGam.api;
 
 import chbbo.BEOhGam.domain.Note;
+import chbbo.BEOhGam.dto.NoteDTO;
 import chbbo.BEOhGam.service.NoteService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,7 +21,12 @@ public class NoteAPIController {
 
     @GetMapping("/list")
     @ResponseBody
-    public List<Note> noteList() {
-        return  noteService.findAllNote();
+    public List<NoteDTO> noteList() {
+        List<Note> notes = noteService.findAllNote();
+        List<NoteDTO> noteDTOList = new ArrayList<>();
+        for (Note note : notes) {
+            noteDTOList.add(NoteDTO.toNoteDTO(note));
+        }
+        return noteDTOList;
     }
 }
