592f016dc79f5cd226e46ed1f1f834bf2a0bfa03
SongJSeop <wnstjq7979@gmail.com>
2023-07-03 09:11:44 +0900

    Test: NoteServiceTest에서 saveAndFindNoteTest 수정

--- src/test/java/chbbo/BEOhGam/service/NoteServiceTest.java
+++ src/test/java/chbbo/BEOhGam/service/NoteServiceTest.java
@@ -1,10 +1,14 @@
 package chbbo.BEOhGam.service;
 
 import chbbo.BEOhGam.domain.Note;
+import chbbo.BEOhGam.domain.Text;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,13 +23,25 @@ public class NoteServiceTest {
     void saveAndFindNoteTest() {
         // given
         Note note = new Note();
+        Text text1 = new Text();
+        text1.setContent("hi");
+        Text text2 = new Text();
+        text2.setContent("hello");
+        Text text3 = new Text();
+        text3.setContent("ntmu");
 
         // when
+        List<Text> text = new ArrayList<>();
+        text.add(text1);
+        text.add(text2);
+        text.add(text3);
+        note.setText(text);
+
         noteService.save(note);
         Note foundNote = noteService.findNote(note.getId());
 
         // then
-        assertThat(foundNote).isNotNull();
         assertThat(foundNote).isSameAs(note);
+        assertThat(foundNote.getText()).isSameAs(note.getText());
     }
 }
