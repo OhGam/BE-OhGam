416c053d932ed7691ea86a9c7917105d3de805e1
SongJSeop <wnstjq7979@gmail.com>
2023-07-06 20:38:55 +0900

    Feat: 노트 작성 API 구현

--- src/main/java/chbbo/BEOhGam/api/NoteAPIController.java
+++ src/main/java/chbbo/BEOhGam/api/NoteAPIController.java
@@ -2,6 +2,7 @@ package chbbo.BEOhGam.api;
 
 import chbbo.BEOhGam.domain.Note;
 import chbbo.BEOhGam.dto.NoteDTO;
+import chbbo.BEOhGam.service.MemberService;
 import chbbo.BEOhGam.service.NoteService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,8 @@ import java.util.List;
 public class NoteAPIController {
 
     private final NoteService noteService;
+    private final MemberService memberService;
+
 
     // 조회 api!
     // 모든 감사 노트 목록 조회하는 api
@@ -45,5 +48,19 @@ public class NoteAPIController {
             noteDTOList.add(NoteDTO.toNoteDTO(note));
         }
         return ResponseEntity.ok().body(noteDTOList);
+    }
+
+    // 등록 api!
+    // 회원 로그인 아이디를 받아 감사 노트를 작성하는 api
+    @PostMapping("/write")
+    public ResponseEntity<NoteDTO> write(@RequestParam String userId, @RequestBody NoteDTO noteDTO) {
+        System.out.println(userId);
+        System.out.println(noteDTO);
+        noteDTO.setLikes(0);
+        noteDTO.setViews(0);
+        Note note = Note.toNote(noteDTO);
+        note.setMember(memberService.findByUserId(userId));
+        noteService.save(note);
+        return ResponseEntity.ok().body(noteDTO);
     }
 }
