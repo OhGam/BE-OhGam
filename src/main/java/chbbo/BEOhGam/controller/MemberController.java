fb38f2870c5b830e22696babd795f6ad75d9980b
pia01190 <pia01190@naver.com>
2023-07-07 22:22:02 +0900

    Feat: loginUser 메서드 구현

--- src/main/java/chbbo/BEOhGam/controller/MemberController.java
+++ src/main/java/chbbo/BEOhGam/controller/MemberController.java
@@ -8,6 +8,8 @@ import chbbo.BEOhGam.validator.CheckUserIdValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -70,6 +72,13 @@ public class MemberController {
     public String loginForm() {
 
         return "loginForm";
+    }
+
+    @GetMapping("/")
+    public String loginUser(Model model, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("info", userDetails.getUsername());
+        return "loginUser";
     }
 
 }
